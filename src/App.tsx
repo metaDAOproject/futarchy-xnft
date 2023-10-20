be486394213dd1fa539bb6c127916917a378d787
metaproph3t <metaproph3t@protonmail.com>
2023-10-20 00:00:00 +0000

    Add pass and fail markets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, FlatList, ActivityIndicator, ScrollView } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList, ActivityIndicator, ScrollView } from "react-native";
 import { registerRootComponent } from "expo";
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
@@ -12,29 +12,6 @@ import { useSolanaProvider } from "./hooks/xnft-hooks";
 import { AutocratV0 } from "./idl/autocrat_v0";
 import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 const AutocratIDL: AutocratV0 = require("./idl/autocrat_v0.json");
-
-let static_proposals = [
-  {
-    title: "Proposal 0 - Put upgrades behind a timelock",
-    program_id: "ATn9hbwGBFjcihRJ2a5BMJ9T8a9nyeXH54A3JWouAu6h",
-    calldata: "0x4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f",
-    accounts: [
-      { pubkey: "9tXPRcvUJ12kR32KGB43yvM4RDvR53gM2JStZGUnftfa", is_writable: true, is_signer: false },
-      { pubkey: "4VtKJ3JNK9rXbRHVb3eg9Q2Q8brZqGJDtZgrMzrWcmuC", is_writable: false, is_signer: true },
-      { pubkey: "K5KJ3JNK9rXbRHVb3eg9Q2Q8brZqGJDtZgrMzrWcmuA", is_writable: true, is_signer: true },
-    ],
-  },
-  {
-    title: "Proposal 1 - Leveraged short ETH",
-    program_id: "BcYhbwGBFjcihRJ2a5BMJ9T8a9nyeXH54A3JWouAu6h",
-    calldata: "0x1234567890abcdef",
-    accounts: [
-      { pubkey: "3gXPRcvUJ12kR32KGB43yvM4RDvR53gM2JStZGUnftfa", is_writable: true, is_signer: false },
-      { pubkey: "5VtKJ3JNK9rXbRHVb3eg9Q2Q8brZqGJDtZgrMzrWcmuC", is_writable: false, is_signer: true },
-      { pubkey: "P5KJ3JNK9rXbRHVb3eg9Q2Q8brZqGJDtZgrMzrWcmuA", is_writable: true, is_signer: true },
-    ],
-  },
-];
 
 const AUTOCRAT_PROGRAM_ID = new anchor.web3.PublicKey(
   "Ctt7cFZM6K7phtRo5NvjycpQju7X6QTSuqNen2ebXiuc"
@@ -65,6 +42,8 @@ export function App() {
     });
   }, [provider]); // provider gets asynchronously set, so we need to wait for it to be set before we can use it
 
+ 
+
   return (
     <View style={{ flex: 1, backgroundColor: "#222", padding: 10 }}>
       <Text style={{ fontSize: 20, color: "#fff", textAlign: "left", paddingTop: 8, paddingLeft: 15 }}>Proposals</Text>
@@ -82,7 +61,55 @@ export function App() {
           >
             <Text style={styles.proposalText}>Proposal {item.account.number}</Text>
             {selectedProposal === item && (
-              <View style={styles.proposalDescription}>
+              <View style={{...styles.proposalDescription, flexDirection: 'column'}}>
+                <View style={{flex: 1}}>
+                  <View style={styles.marketBox}>
+                    <Text style={styles.marketText}>Pass Market</Text>
+                    <Text style={styles.priceText}>Price: $10</Text>
+                    <View style={styles.amountBox}>
+                      <View style={styles.amountItem}>
+                        <Text style={styles.amountLabel}>You Pay</Text>
+                        <TouchableOpacity activeOpacity={1} onPress={() => {}}>
+                          <TextInput
+                            style={styles.amountInput}
+                            keyboardType="numeric"
+                            placeholder="Enter amount"
+                            // You can add state to manage the value entered by the user
+                            // For example: value={payValue} onChangeText={(text) => setPayValue(text)}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                      <View style={styles.amountItem}>
+                        <Text style={styles.amountLabel}>You Receive</Text>
+                        <Text style={styles.amountValue}>10 Tokens</Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+                <View style={{flex: 1}}>
+                  <View style={styles.marketBox}>
+                    <Text style={styles.marketText}>Fail Market</Text>
+                    <Text style={styles.priceText}>Price: $8</Text>
+                    <View style={styles.amountBox}>
+                      <View style={styles.amountItem}>
+                        <Text style={styles.amountLabel}>You Pay</Text>
+                        <TouchableOpacity activeOpacity={1} onPress={() => {}}>
+                          <TextInput
+                            style={styles.amountInput}
+                            keyboardType="numeric"
+                            placeholder="Enter amount"
+                            // You can add state to manage the value entered by the user
+                            // For example: value={payValue} onChangeText={(text) => setPayValue(text)}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                      <View style={styles.amountItem}>
+                        <Text style={styles.amountLabel}>You Receive</Text>
+                        <Text style={styles.amountValue}>5 Tokens</Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
                 <Text style={styles.jsonLabel}>Program ID</Text>
                 <Text style={styles.jsonText}>
                   {item.account.instruction.programId.toString()}
@@ -94,8 +121,8 @@ export function App() {
                 <Text style={styles.jsonLabel}>Accounts</Text>
                 <Text style={styles.jsonText}>
                   {JSON.stringify(item.account.instruction.accounts, null, 2)}
-                </Text>
-              </View>
+                </Text> 
+              </View>                
             )}
           </TouchableOpacity>
         )}
@@ -123,6 +150,62 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 8,
     marginTop: 10,
+    flexDirection: "row",
+  },
+  marketBoxContainer: {
+    flexDirection: 'row', // This will make the boxes sit side by side
+    justifyContent: 'space-between', // Add space between boxes
+    flex: 1,
+  },
+  marketBox: {
+    backgroundColor: "#555",
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 10,
+    flex: 1,
+  },
+  amountInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 5,
+    marginBottom: 5,
+    color: '#fff',
+  },
+
+  marketText: {
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+
+  priceText: {
+    color: "#fff",
+  },
+
+  amountBox: {
+    marginTop: 10, // Add space between market text and "You Pay" element
+  },
+
+  amountItem: {
+    marginBottom: 10, // Add space between items
+  },
+
+  amountLabel: {
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+
+  amountValue: {
+    color: "#fff",
+  },
+  marketInfo: {
+    backgroundColor: "#555", // Lighter shade for the market info box
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 10, // Added marginBottom for spacing
   },
   descriptionTitle: {
     color: "#fff",
